25039637aed6ce94328ac2995dc6c9395d83d79b
ben8328 <ben.miller8328@gmail.com>
2025-01-22 12:33:28 +1100

    Created working Simulink Model which displays simulation using MATLAB Animation Block

--- main.m
+++ main.m
@@ -22,8 +22,8 @@ sat.J1 = 2.5;    % kg.sqm
 sat.B = 1.17;    % Nms
 
 sat.I_x = 10;   % kg*m^2
-sat.I_y = 12;   % kg*m^2
-sat.I_z = 8;    % kg*m^2
+sat.I_y = 10;   % kg*m^2
+sat.I_z = 10;    % kg*m^2
 
 
 %% Transfer Functions
@@ -33,35 +33,44 @@ motor.tf_den = [motor.Jrw_x, motor.b + (motor.Ke*motor.Kt)/motor.Ra];
 
 
 
+
 %% Stability Analysis
 % Obtained from Transfer Functions
-M = tf(motor.tf_num, motor.tf_den)
-% S = tf(1, [2.5 1.17 0]);
-
-% First find Poles
-motor_pole = pole(M)    % Both negative is Stable
-% sat_pole = pole(S)
-
-% root Locus
-figure()
-rlocus(M)
+% M = tf(motor.tf_num, motor.tf_den)
+% % S = tf(1, [2.5 1.17 0]);
+% 
+% % First find Poles
+% motor_pole = pole(M)    % Both negative is Stable
+% % sat_pole = pole(S)
+% 
+% % root Locus
 % figure()
-% rlocus(S)
-
-% Bode Plot
-figure()
-margin(M)
-
+% rlocus(M)
+% % figure()
+% % rlocus(S)
+% 
+% % Bode Plot
 % figure()
-% margin(S)
-
-% Feedback loop
-figure()
-X_m = feedback(M,1)
-step(X_m)
+% margin(M)
+% 
+% % figure()
+% % margin(S)
+% 
+% % Feedback loop
+% figure()
+% X_m = feedback(M,1)
+% step(X_m)
 
 % figure()
 % X_s = feedback(S,1)
 % step(X_s)
 
 % NOTE: Slow response time is ok
+
+%% simulation Data
+time = 40;
+
+% Desired Input Angles
+phi_in = (45)*(pi/180)      % Roll Angle [Rads]
+theta_in = (45)*(pi/180)    % Pitch Angle [Rads]
+psi_in = (0)*(pi/180)       % Yaw Angle [Rads]
