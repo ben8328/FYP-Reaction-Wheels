1b6a86bf120a19597573c39721f9b56c429b304f
ben8328 <ben.miller8328@gmail.com>
2025-03-08 17:28:42 +1100

    flightGear run files

---
+++ runfg2.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+cd /Applications/FlightGear.app/Contents/Resources
+
+export FG_ROOT=/Applications/FlightGear.app
+
+./../MacOS/fgfs --fdm=null --native-fdm=socket,in,30,localhost,5502,udp  --enable-terrasync --prop:/sim/rendering/shaders/quality-level=0 --aircraft=c172p --fog-fastest --disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound --in-air --airport=YSSY --runway= --altitude=7224 --heading=113 --offset-distance=4.72 --offset-azimuth=0   &
